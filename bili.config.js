05c2b749f1aafa9cb441d3a449eb880953775ef3
Giovane Souza <giovane.bds@gmail.com>
2019-04-29 23:22:39 -0300

    using bili v3

--- bili.config.js
+++ bili.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   input: 'src/vuex-storage-plugin.js',
   format: ['umd', 'umd-min', 'cjs', 'es'],
-  filename: 'vuex-storage-plugin[suffix].js'
+  filename: 'vuex-storage-plugin[suffix].js',
+  compress: true
 };
